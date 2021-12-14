1c6241b26770c62f36adf82dd890810545f22324
tsubanghia2000 <nghia4lu@gmail.com>
2021-12-14 10:21:22 +0700

    edit layout home

--- QuanLiSua/Models/QuanLiSua.Designer.cs
+++ QuanLiSua/Models/QuanLiSua.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\asus\OneDrive\Máy tính\QuanLiSua\QuanLiSua\Models\QuanLiSua.edmx'. 
+﻿// T4 code generation is enabled for model 'D:\DO_AN_2\doan2\QuanLiSua\Models\QuanLiSua.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
